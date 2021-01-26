4193cb2e2726ecad7adaf1473b1f22c624557267
Long Nguyen <anonyhost@gmail.com>
2021-01-26 09:08:44 +0700

    change internal endpoint

--- src/app/pages/vnu-schedule/vnu-schedule.const.ts
+++ src/app/pages/vnu-schedule/vnu-schedule.const.ts
@@ -1,5 +1,5 @@
 export const host = 'http://localhost:8080';
-export const externalContext = '/internal';
+export const externalContext = '/external';
 export const scheduleContext = '/schedule/v1/schedules';
 export const getScheduleEndpoint = '/schedule';
 export const getTermEndpoint = '/term';
